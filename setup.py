c5304ea85ec5f7d2e5e3d147e3aeb9760155dac6
FARBEX97 <64985555+FARBEX97@users.noreply.github.com>
2021-01-09 11:50:21 +0100

    v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="coffeece",
-    version="0.2.0",
+    version="0.2.1",
     author="FARBEX97",
     author_email="fernandoarbexcv@gmail.com",
     description="Python useful classes for office work automation",
